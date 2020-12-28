bf484e77b16e360f5c40de38f02a734c3c848e07
Andre Postiga <andre.postiga@msn.com>
2020-12-28 10:28:21 -0300

    refactor: entities less anemic

--- src/AspNetStandard.Diagnostics.HealthChecks/Entities/HealthCheckResponse.cs
+++ src/AspNetStandard.Diagnostics.HealthChecks/Entities/HealthCheckResponse.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace AspNetStandard.Diagnostics.HealthChecks.Entities
 {
@@ -9,8 +10,36 @@ namespace AspNetStandard.Diagnostics.HealthChecks.Entities
             Entries = new Dictionary<string, HealthCheckResultExtended>();
         }
 
-        public HealthStatus OverAllStatus { get; set; }
-        public long? TotalResponseTime { get; set; }
         public IDictionary<string, HealthCheckResultExtended> Entries { get; }
+
+        public HealthStatus OverAllStatus
+        {
+            get
+            {
+                var status = HealthStatus.Healthy;
+                foreach (var healthCheckResultExtended in Entries.Values)
+                {
+                    if (healthCheckResultExtended.Status == HealthStatus.Unhealthy)
+                    {
+                        status = HealthStatus.Unhealthy;
+                        break;
+                    }
+
+                    if (healthCheckResultExtended.Status == HealthStatus.Degraded)
+                    {
+                        status = HealthStatus.Degraded;
+                    }
+                }
+                return status;
+            }
+        }
+
+        public long? TotalResponseTime
+        {
+            get
+            {
+                return Entries.Values.Sum(c => c.ResponseTime);
+            }
+        }
     }
 }
--- src/AspNetStandard.Diagnostics.HealthChecks/Entities/HealthCheckResultExtended.cs
+++ src/AspNetStandard.Diagnostics.HealthChecks/Entities/HealthCheckResultExtended.cs
@@ -16,17 +16,5 @@ namespace AspNetStandard.Diagnostics.HealthChecks.Entities
 
         [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public DateTime LastExecutionUtc { get; set; }
-
-        public override bool Equals(object obj)
-        {
-            if (obj == null) return false;
-
-            var item = obj as HealthCheckResultExtended;
-            return (
-                this.Description == item.Description
-                && this.Exception == item.Exception
-                && this.Status == item.Status
-            );
-        }
     }
 }
