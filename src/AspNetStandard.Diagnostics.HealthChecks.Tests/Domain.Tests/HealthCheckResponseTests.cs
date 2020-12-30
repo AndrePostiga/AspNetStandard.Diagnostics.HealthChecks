5ca58f8cd3159871a898ddb1d4dd6e1b17d14de8
Andre Postiga <andre.postiga@msn.com>
2020-12-30 16:15:30 -0300

    tests: add Authentication and Domain.HCResponse tests

---
+++ src/AspNetStandard.Diagnostics.HealthChecks.Tests/Domain.Tests/HealthCheckResponseTests.cs
@@ -0,0 +1,68 @@
+﻿using AspNetStandard.Diagnostics.HealthChecks.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace AspNetStandard.Diagnostics.HealthChecks.Tests.Domain.Tests
+{
+    public class HealthCheckResponseTests
+    {
+        HealthCheckResultExtended HealthyResult;
+        HealthCheckResultExtended DegradedResult;
+        HealthCheckResultExtended UnhealthyResult;
+
+        public HealthCheckResponseTests()
+        {
+            HealthyResult = new HealthCheckResultExtended(new HealthCheckResult(HealthStatus.Healthy)) { ResponseTime = 1};
+            DegradedResult = new HealthCheckResultExtended(new HealthCheckResult(HealthStatus.Degraded)) { ResponseTime = 1 };
+            UnhealthyResult = new HealthCheckResultExtended(new HealthCheckResult(HealthStatus.Unhealthy)) { ResponseTime = 1 };
+        }
+
+        [Fact(DisplayName = "Should return degraded if at least one status is degraded")]
+        public void ShouldReturnDegraded()
+        {
+            var sut = new HealthCheckResponse();
+            sut.Entries.Add("AnyName", HealthyResult);
+            sut.Entries.Add("AnyAnotherName", DegradedResult);
+
+            Assert.Equal(HealthStatus.Degraded, sut.OverAllStatus);
+
+        }
+
+        [Fact(DisplayName = "Should return unhealthy if at least one status is unhealthy")]
+        public void ShouldReturnUnhealthy()
+        {
+            var sut = new HealthCheckResponse();
+            sut.Entries.Add("AnyName", HealthyResult);
+            sut.Entries.Add("AnyAnotherName", DegradedResult);
+            sut.Entries.Add("UnhealthyAnotherName", UnhealthyResult);
+
+            Assert.Equal(HealthStatus.Unhealthy, sut.OverAllStatus);
+
+        }
+
+        [Fact(DisplayName = "Should return healthy if all status is healthy")]
+        public void ShouldReturnHealthy()
+        {
+            var sut = new HealthCheckResponse();
+            sut.Entries.Add("AnyName", HealthyResult);
+            sut.Entries.Add("AnyAnotherName", HealthyResult);
+            sut.Entries.Add("UnhealthyAnotherName", HealthyResult);
+
+            Assert.Equal(HealthStatus.Healthy, sut.OverAllStatus);
+
+        }
+
+        [Fact(DisplayName = "Should sum correctly totalResponseTime")]
+        public void ShouldSumCorrectly()
+        {
+            var sut = new HealthCheckResponse();
+            sut.Entries.Add("AnyName", HealthyResult);
+            sut.Entries.Add("AnyAnotherName", DegradedResult);
+            sut.Entries.Add("UnhealthyAnotherName", UnhealthyResult);
+
+            Assert.Equal(3, sut.TotalResponseTime);
+        }
+    }
+}
---
+++ src/AspNetStandard.Diagnostics.HealthChecks.Tests/Services.Tests/AuthenticationServiceTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Moq;
+using System.Net.Http;
+using System.Collections.Specialized;
+using AspNetStandard.Diagnostics.HealthChecks.Services;
+
+namespace AspNetStandard.Diagnostics.HealthChecks.Tests.Services.Tests
+{
+    public class AuthenticationServiceTests
+    {
+        HttpRequestMessage httpMessage;
+        public AuthenticationServiceTests()
+        {
+            httpMessage = new HttpRequestMessage()
+            {
+                RequestUri = new Uri("http://anyDomain/health?ApiKey=AnyApiKey")
+            };
+        }
+
+        [Fact(DisplayName ="Should return true when apiKey is correctly")]
+        public void ShoudlReturnTrueOnValidation()
+        {
+            var builder = new HealthChecksBuilder().UseAuthorization("AnyApiKey");
+            var sut = new AuthenticationService(builder);
+
+            var act = sut.ValidateApiKey(httpMessage);
+
+            Assert.True(act == true);
+        }
+
+        [Fact(DisplayName = "Should validate if authorization is needed")]
+        public void ShoudlReturnTrueIfNeedValidation()
+        {
+            var builder = new HealthChecksBuilder().UseAuthorization("AnyApiKey");
+            var sut = new AuthenticationService(builder);
+
+            var act = sut.NeedAuthentication();
+
+            Assert.True(act == true);
+        }
+
+        [Fact(DisplayName = "Should validate if authorization is not needed")]
+        public void ShoudlReturnTrueIfNoNeedValidation()
+        {
+            var builder = new HealthChecksBuilder();
+            var sut = new AuthenticationService(builder);
+
+            var act = sut.NeedAuthentication();
+
+            Assert.True(act == false);
+        }
+    }
+}
