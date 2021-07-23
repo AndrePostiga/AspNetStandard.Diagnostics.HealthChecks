702876a99d10bfc6d3705db939bcc4b544576697
HelenMinin <helenminis@gmail.com>
2021-07-23 18:02:32 -0300

    POC de Health Check com WCF funcional com sql server

---
+++ src/AspNetStandard.Diagnostics.HealthChecksWcf/HealthCheckResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetStandard.Diagnostics.HealthChecksWcf
+{
+    [DataContract]
+    public class HealthCheckResponse
+    {
+        [DataMember]
+        public string Status { get; set; }
+    }
+}
