321c849ef8d6efe2ee92847d668cc68c41ca41e3
HelenMinin <helenminis@gmail.com>
2021-08-04 14:42:29 -0300

    Alteracao solicitada pelo Postiga

--- src/AspNetStandard.Diagnostics.HealthChecksWcf/WcfHealthCheckRouteExtension.cs
+++ src/AspNetStandard.Diagnostics.HealthChecksWcf/WcfHealthCheckRouteExtension.cs
@@ -3,11 +3,12 @@ using System.Web.Routing;
 
 namespace AspNetStandard.Diagnostics.HealthChecksWcf
 {
-    public static class WcfHeathCheckRouteExtension
+    public static class WcfHealthCheckRouteExtension
+
     {
-        public static WcfHealthCheckBuilder AddWcfHealthCheck()
+        public static WcfHealthCheckBuilder AddWcfHealthCheck(string healthEndpoint = "health/")
         {
-            RouteTable.Routes.Add(new ServiceRoute("health/", new WebServiceHostFactory(),
+            RouteTable.Routes.Add(new ServiceRoute(healthEndpoint, new WebServiceHostFactory(),
               typeof(WcfHealthCheckService)));
 
             return new WcfHealthCheckBuilder();
